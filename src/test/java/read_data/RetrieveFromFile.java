bcef39177416e680e8ed43d8e286a694f2844b40
rojochris409 <rojochristian409@gmail.com>
2023-02-11 13:45:20 -0600

    Added read from excel logic.

---
+++ src/test/java/read_data/RetrieveFromFile.java
@@ -0,0 +1,77 @@
+package read_data;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class RetrieveFromFile {
+
+    @Test
+    public void readFileTest() throws IOException {
+
+        File excelFile = new File("src/test/resources/TestSetUp.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(excelFile);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet page1 = workbook.getSheet("Sheet1");
+        XSSFRow row1 = page1.getRow(0);
+        XSSFCell cell1 = row1.getCell(0);
+        System.out.println(cell1);
+
+        XSSFRow row2 = page1.getRow(1);
+        XSSFCell cell2= row2.getCell(0);
+        System.out.println(cell2);
+    }
+
+    @Test
+    public void readFileTest2() throws IOException {
+        File excelFile = new File("src/test/resources/TestSetUp.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(excelFile);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet1 = workbook.getSheetAt(0);
+        XSSFRow row1 = sheet1.getRow(0);
+
+        for (int i = row1.getFirstCellNum(); i < row1.getLastCellNum(); i++) {
+
+            XSSFCell cell = row1.getCell(i);
+            System.out.print(cell + " | ");
+        }
+    }
+
+
+        @Test
+        public void getAllData () throws IOException {
+            File excelFile = new File("src/test/resources/TestSetUp.xlsx");
+            FileInputStream fileInputStream = new FileInputStream(excelFile);
+            XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+            XSSFSheet sheet1 = workbook.getSheet("Sheet1");
+
+            for (int i = sheet1.getFirstRowNum(); i < sheet1.getLastRowNum(); i++) {
+                XSSFRow tempRow = sheet1.getRow(i);
+
+                for (int j = tempRow.getFirstCellNum(); j <tempRow.getLastCellNum() ; j++) {
+                    System.out.print(tempRow.getCell(j) + " | ");
+                }
+                System.out.println();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+}
